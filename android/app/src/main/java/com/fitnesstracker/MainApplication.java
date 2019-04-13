5709f1923d5518a3e073caa96002e088efebec2a
Nick Rodgers <nick.rodgers42@gmail.com>
2019-04-13 10:56:22 -0600

    Add react-native-vector-icons and eject native-base theme

--- android/app/src/main/java/com/fitnesstracker/MainApplication.java
+++ android/app/src/main/java/com/fitnesstracker/MainApplication.java
@@ -3,6 +3,7 @@ package com.fitnesstracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new MapsPackage(),
             new RNCameraPackage()
       );
